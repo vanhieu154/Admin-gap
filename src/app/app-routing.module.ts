11f18b8bf4610080959c0f6711d981a92227fba1
thuynthuyn <118806972+thuynthuyn@users.noreply.github.com>
2023-04-24 13:47:10 +0700

    thêm component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:"customer-detail",component:CustomerDetailComponent
+  },
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
