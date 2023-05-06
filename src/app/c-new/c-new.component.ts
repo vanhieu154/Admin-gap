6d101d8fd71ab46ea255e95f1145ce4d5a331995
thaothanh246 <119411244+thaothanh246@users.noreply.github.com>
2023-05-06 23:52:44 +0700

    Update

--- src/app/c-new/c-new.component.ts
+++ src/app/c-new/c-new.component.ts
@@ -7,6 +7,8 @@ import { Coupon } from '../admin-coupon/coupon';
 import { FormControl, FormGroup } from '@angular/forms';
 import { SuccessDialogComponent } from '../success-dialog/success-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Product } from '../admin-product/product';
+import { ProductApiService } from '../admin-product/product-api.service';
 @Component({
   selector: '[app-c-new]',
   templateUrl: './c-new.component.html',
@@ -18,9 +20,11 @@ export class CNewComponent {
   selectCouponId: string='';
   coupon=new Coupon();
   coupons:any;
+  products:any;
+  product=new Product();
   selectedFiles: File[] = [];
   errMessage:string=''
-  constructor(public _service: CouponApiService, private http: HttpClient,private router:Router,public dialog: MatDialog){
+  constructor(public _service: CouponApiService,private _pservice: ProductApiService, private http: HttpClient,private router:Router,public dialog: MatDialog){
   this._service.getCoupons().subscribe({
   next:(data)=>{this.coupons=data},
   error:(err)=>{this.errMessage=err}
@@ -30,6 +34,7 @@ export class CNewComponent {
   }
 
   postCoupon()  {
+    this.coupon.MaCoupon=this.coupons.length+1;
     this.coupon.cDate= new Date(Date.now())
     this._service.postCoupon(this.coupon).subscribe({
       next:(data)=>{this.coupon=data},
@@ -41,7 +46,8 @@ export class CNewComponent {
       height: '220px',
     });
     dialogRef.afterClosed().subscribe(() => {
-      // refresh product list
+      this.router.navigate(['coupons'])
+
     });
   }
   onFileSelected(event: any,coupon:Coupon) {
@@ -60,9 +66,31 @@ export class CNewComponent {
 
   }
 
-  onDeleteImage(index: number) {
-    this.coupon.Hinhanh.splice(index, 1);
+  displayTable = false;
+  showTable() {
+    this.displayTable = true;
+    this.getProducts();
   }
+  getProducts(){
+    this._pservice.getProducts().subscribe({
+      next:(data)=>{this.products=data;  },
+      error:(err)=>{this.errMessage=err}
+      })
+  }
+
+
+  filteredProducts: any[] | undefined;
+  public searchProduct:string=''
+
+  filterProducts() {
+     this.filteredProducts = this.products.filter((product:  { MaSP: string; TenSP: string; Price: number;LoaiSP :string; }) =>
+     product.MaSP.toString().includes(this.searchProduct.toLowerCase()) ||
+     product.TenSP.toLowerCase().includes(this.searchProduct.toLowerCase()) ||
+     product.LoaiSP.toLowerCase().includes(this.searchProduct.toLowerCase()) ||
+     product.Price.toString().toLowerCase().includes(this.searchProduct.toLowerCase()));
+  }
+
+
 
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
--- src/app/pro-new/pro-new.component.ts
+++ src/app/pro-new/pro-new.component.ts
@@ -8,6 +8,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { Promotion } from '../admin-promotion/Promotion';
 import { PromotionService } from '../admin-promotion/promotion.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../admin-product/product';
+import { ProductApiService } from '../admin-product/product-api.service';
 @Component({
   selector: 'app-pro-new',
   templateUrl: './pro-new.component.html',
@@ -18,13 +20,15 @@ export class ProNewComponent {
   selectPromotionId: string='';
   promotion=new Promotion();
   promotions:any;
+  products:any;
+  product=new Product();
   id: any;
   errMessage:string='';
   types=["Giảm giá loại sản phẩm","Đồng giá"]
 
 
 
-  constructor(public _service: PromotionService, private http: HttpClient,private router:Router, public dialog: MatDialog,private activateRoute:ActivatedRoute){
+  constructor(public _service: PromotionService,private _pservice: ProductApiService, private http: HttpClient,private router:Router, public dialog: MatDialog,private activateRoute:ActivatedRoute){
     this._service.getPromotions().subscribe({
       next:(data)=>{this.promotions=data},
       error:(err)=>{this.errMessage=err}
@@ -43,7 +47,7 @@ postPromotion()  {
     height: '220px',
   });
   dialogRef.afterClosed().subscribe(() => {
-    // refresh product list
+    this.router.navigate(['promotions'])
   });
 }
 
@@ -52,6 +56,31 @@ range = new FormGroup({
   start: new FormControl<Date | null>(null),
   end: new FormControl<Date | null>(null),
 });
+
+displayTable = false;
+  showTable() {
+    this.displayTable = true;
+    this.getProducts();
+  }
+  getProducts(){
+    this._pservice.getProducts().subscribe({
+      next:(data)=>{this.products=data;  },
+      error:(err)=>{this.errMessage=err}
+      })
+  }
+
+
+  filteredProducts: any[] | undefined;
+  public searchProduct:string=''
+
+  filterProducts() {
+     this.filteredProducts = this.products.filter((product:  { MaSP: string; TenSP: string; Price: number;LoaiSP :string; }) =>
+     product.MaSP.toString().includes(this.searchProduct.toLowerCase()) ||
+     product.TenSP.toLowerCase().includes(this.searchProduct.toLowerCase()) ||
+     product.LoaiSP.toLowerCase().includes(this.searchProduct.toLowerCase()) ||
+     product.Price.toString().toLowerCase().includes(this.searchProduct.toLowerCase()));
+  }
+
 
 toMainPromotionPage(){
   this.router.navigate(['promotions'])
